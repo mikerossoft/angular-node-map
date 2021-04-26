4f286541c0485d138ed160ca7bdf21eb95debb09
DESKTOP-0BJOJIV\Indicium Dynamics <seksky333@gmail.com>
2021-04-26 11:31:17 +1000

    Remove extra caller component Update to use default app component to call node-map component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NodeMapComponent } from './node-map/node-map.component';
-import { CallerComponent } from './caller/caller.component';
 
 @NgModule({
-    declarations: [AppComponent, NodeMapComponent, CallerComponent],
+    declarations: [AppComponent, NodeMapComponent],
     imports: [BrowserModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
